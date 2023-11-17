3ef08ff97bdffd90e7139ed4c08dbe986997e9d8
Phuong Le Do <145152948+lphngdo@users.noreply.github.com>
2023-11-17 21:33:38 +0700

    SQL Basic - Medium Solutions

---
+++ 02. SQL Basic Medium/01. Weather Observation Station 18.sql
@@ -0,0 +1,6 @@
+-- Solution: MS SQL Server
+SELECT FORMAT(ROUND((MAX(LAT_N) - MIN(LAT_N)) + (MAX(LONG_W) - MIN(LONG_W)), 4), '##.####')
+  FROM STATION;
+-- Solution: MySQL
+SELECT ROUND(((MAX(LAT_N) - MIN(LAT_N)) + (MAX(LONG_W) - MIN(LONG_W))), 4)
+  FROM STATION;
