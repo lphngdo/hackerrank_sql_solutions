580425eb9035bbbd001ab012f245652fdbd7d823
Phuong Le Do <145152948+lphngdo@users.noreply.github.com>
2023-11-17 21:05:27 +0700

    Delete 01. SQL Basic Easy directory

--- 01. SQL Basic Easy/29. Weather Observation Station 2.sql
+++
@@ -1,8 +0,0 @@
--- Solution: MS SQL Server
-SELECT FORMAT(ROUND(SUM(LAT_N), 2), '##.##'),
-       FORMAT(ROUND(SUM(LONG_W), 2), '##.##')
-  FROM STATION;
--- Solution: MySQL
-SELECT ROUND(SUM(LAT_N), 2),
-       ROUND(SUM(LONG_W), 2)
-  FROM STATION;
--- 01. SQL Basic Easy/35. Population Census.sql
+++
@@ -1,5 +0,0 @@
-SELECT SUM(city.POPULATION)
-  FROM CITY AS city
-       INNER JOIN COUNTRY AS country
-               ON city.COUNTRYCODE = country.CODE
- WHERE country.CONTINENT = 'Asia';
